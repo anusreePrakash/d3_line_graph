2b6fa1c8c3f11105996cdeca8b750f0e1f10a7c9
Anusree Prakash <anusree@thoughtworks.com>
2016-11-17 18:41:57 +0530

    implemented sine graph with random equation

--- sineGraph/visualize.js
+++ sineGraph/visualize.js
@@ -6,20 +6,8 @@ const MARGIN = 30;
 const INNER_WIDTH = WIDTH - 2 * MARGIN;
 const INNER_HEIGHT = HEIGHT - 2 * MARGIN;
 
-var curveArray = [
-    {"d3Curve":d3.curveLinear,"curveTitle":"curveLinear"},
-    {"d3Curve":d3.curveLinearClosed,"curveTitle":"curveLinear"},
-    {"d3Curve":d3.curveStep,"curveTitle":"curveStep"},
-    {"d3Curve":d3.curveBasis,"curveTitle":"curveBasis"},
-    {"d3Curve":d3.curveBundle.beta(0.7),"curveTitle":"curveBasis"},
-    {"d3Curve":d3.curveCardinalClosed,"curveTitle":"curveCardinal"},
-    {"d3Curve":d3.curveCardinal,"curveTitle":"curveCardinal"},
-    {"d3Curve":d3.curveMonotoneX,"curveTitle":"curveMonotoneX"},
-  ];
 
-
-
-var display = function (interpolate) {
+var display = function (tension) {
 
   var svg = d3.select('.container').append('svg')
   .attr('width', WIDTH)
@@ -40,9 +28,6 @@ var display = function (interpolate) {
   	return "translate("+x+","+y+")";
   };
 
-  svg.append("title")
-          .text("Value vs Date Graph");
-
   svg.append("g")
       .attr('transform', translate(MARGIN, HEIGHT - MARGIN))
       .call(xAxis);
@@ -51,16 +36,13 @@ var display = function (interpolate) {
       .attr('transform', translate(MARGIN, MARGIN))
       .call(yAxis);
 
-
   var sineLineGroup = svg.append('g')
     .attr('transform',  translate(MARGIN, MARGIN));
-
-
 
   var sineLine = d3.line()
 		.x(function(q){return xScale(q)})
 		.y(function(q){return yScale((Math.sin(3 *q)+1)/2)})
-    // .curve(interpolate.d3Curve);
+    .curve(d3.curveCardinal.tension(tension));
 
     sineLineGroup.append('path')
       .datum(sinePoints)
@@ -77,9 +59,14 @@ var display = function (interpolate) {
     .attr('fill', 'white')
     .attr('stroke-width', '1px')
     .attr('stroke','steelblue');
-
-    HEIGHT = HEIGHT + HEIGHT;
-
 }
 
-window.onload = display;
+var tensions = d3.scaleLinear()
+      .domain([0, 4])
+      .range([-2,1])
+
+window.onload = function () {
+  for (var i = 0; i < 5; i++) {
+    display(tensions(i));
+  }
+}
